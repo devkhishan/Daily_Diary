e29308e3c45e58f5f8ad6dee7f80907d58971e17
devkhishan <devk21111.cd@rmkec.ac.in>
2024-05-20 11:37:01 +0530

    Initializing User Schema

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,7 +1,7 @@
 const express = require('express') 
 const router = express.Router()
 const Post = require('../models/Post')
-
+const User = require('../models/User')
 const adminLayout = '../views/layouts/admin'
 
 router.get('/admin', (req,res) => {
@@ -14,4 +14,14 @@ router.get('/admin', (req,res) => {
 })
 
 
+
+router.post('/admin', (req,res) => {
+    try{
+        const {username, password} = req.body;
+        console.log(req.body); 
+        res.redirect('/admin');
+    }catch(e){
+        console.log(e);
+    }
+})
 module.exports = router;
